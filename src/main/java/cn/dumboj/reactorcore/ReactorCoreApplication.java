9e3c8ae64505c9385b1ec6989e2d545bfe7dc050
DumboJ <dumbojxx@gmail.com>
2022-07-26 01:04:49 +0800

    local init

---
+++ src/main/java/cn/dumboj/reactorcore/ReactorCoreApplication.java
@@ -0,0 +1,13 @@
+package cn.dumboj.reactorcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactorCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/dumboj/reactorcore/ReactorCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.dumboj.reactorcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
